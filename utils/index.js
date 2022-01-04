707e2bee7864d0b41966b14addb11554e1e9336c
Jude Collins <jude.collins13@yahoo.com>
2022-01-04 14:04:57 -0500

    js questions

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,56 @@
-// TODO: Include packages needed for this application
+//  correct imports
+const inquirer = require("inquirer");
+const generatorMarkdown = require("./generateMarkdown")
+const fs = require("fs"); 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//  ask to the user
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "UserName",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email",
+  },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  {
+    type: "input",
+    message: "What is the title for your project?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Please give description of your project.",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "What necessary dependencies must be installed to run this app?",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "What is this app used for?",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "What license was used for this README?",
+    name: "License",
+  },
 
-// TODO: Create a function to initialize app
-function init() {}
+  {
+    type: "input",
+    message: "Please add contributors",
+    name: "Contributor",
+  },
+  {
+    type: "input",
+    message: "What command do you use to test this App?",
+    name: "Test",
+  }
+];
 
-// Function call to initialize app
-init();
